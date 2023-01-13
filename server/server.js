f1b04893ce16a82da9f573bcfe2e940816735d97
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2023-01-14 00:26:42 +0200

    level 200 completed

--- server/server.js
+++ server/server.js
@@ -1,15 +1,63 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 5000;
+const initialVideos = require('./data/exampleresponse.json');
+const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
+const express = require('express');
+const bp = require('body-parser');
 
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = [...initialVideos];
 
-// GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+app.get('/', (req, res) => {
+  res.status(200).send(videos);
+});
+
+app.post('/', (req, res) => {
+  const video = {...req.body};
+  if(!video) {
+    res.status(404).send({
+      'result': 'failure',
+      'message': 'Video could not be saved'
+    });
+    return;
+  };
+
+  const rating = Math.round(Math.random() * 3000);
+  const newDate = new Date();
+  const date = newDate.toLocaleDateString('fr-CA');
+  const time = newDate.getHours() + ':' + newDate.getMinutes() + ':' + newDate.getSeconds();
+  Object.assign(video, {
+    id: uuidv4(),
+    rating: rating, 
+    date: date, 
+    time: time
+  });
+  videos.push(video);
+});
+
+app.get('/:id', (req, res) => {
+  const video = videos.find(video => video.id === +req.params.id);
+  if(!video) {
+    res.status(404).send('Could not find video with this ID');
+    return;
+  };
+  res.status(200).send(video);
+});
+
+app.delete('/:id', (req, res) => {
+  const videoIndex = videos.findIndex(video => video.id === +req.params.id);
+  if(!videoIndex) {
+    res.status(404).send({
+      "result": "failure",
+      "message": "Video could not be deleted"
+    });
+    return;
+  };
+  videos.splice(videoIndex, 1);
+  res.status(200).send(videos);
 });
